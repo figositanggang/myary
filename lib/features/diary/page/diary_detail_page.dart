0fbd63f270c2a21e071002a6b71be35077cc53f2
unlinked <hamonangansitanggang29@gmail.com>
2024-01-17 03:21:08 +0700

    text to speech

--- lib/features/diary/page/diary_detail_page.dart
+++ lib/features/diary/page/diary_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:get/get.dart';
 import 'package:myary/features/diary/get_controllers/diary_detail_controller.dart';
 import 'package:myary/features/diary/models/diary_model.dart';
@@ -8,7 +9,7 @@ import 'package:myary/helpers/firebase_helper.dart';
 import 'package:myary/utils/custom_methods.dart';
 import 'package:myary/utils/custom_widgets.dart';
 
-class DiaryDetailPage extends StatelessWidget {
+class DiaryDetailPage extends StatefulWidget {
   final String diaryId;
   final DiaryModel diaryModel;
   final Color bgColor;
@@ -21,18 +22,66 @@ class DiaryDetailPage extends StatelessWidget {
     required this.diaryId,
   });
 
+  @override
+  State<DiaryDetailPage> createState() => _DiaryDetailPageState();
+}
+
+class _DiaryDetailPageState extends State<DiaryDetailPage> {
   final DiaryDetailController detailController =
       Get.put(DiaryDetailController());
   final UserController userController = Get.put(UserController());
 
+  late FlutterTts flutterTts;
+  List<Map> _voices = [];
+  Map? _currentVoice;
+
+  @override
+  void initState() {
+    super.initState();
+
+    flutterTts = FlutterTts();
+
+    initTTS();
+  }
+
+  @override
+  void dispose() {
+    try {
+      flutterTts.stop();
+    } catch (e) {}
+    super.dispose();
+  }
+
+  void initTTS() {
+    flutterTts.getVoices.then((data) {
+      try {
+        List<Map> voices = List<Map>.from(data);
+        setState(() {
+          _voices =
+              voices.where((voice) => voice["name"].contains("id")).toList();
+          _currentVoice = _voices[2];
+          setVoice(_currentVoice!);
+        });
+      } catch (e) {
+        print(e);
+      }
+    });
+  }
+
+  void setVoice(Map voice) {
+    flutterTts.setVoice({"name": voice["name"], "locale": voice["locale"]});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: bgColor,
+      backgroundColor: widget.bgColor,
+
+      // @ App Bar
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        foregroundColor: textColor,
-        title: Text(diaryModel.title),
+        foregroundColor: widget.textColor,
+        title: Text(widget.diaryModel.title),
         actions: [
           // @ Edit Diary Button
           IconButton(
@@ -40,8 +89,8 @@ class DiaryDetailPage extends StatelessWidget {
               Navigator.pushReplacement(
                 context,
                 MyRoute(EditDiaryPage(
-                  diaryId: diaryId,
-                  diaryModel: diaryModel,
+                  diaryId: widget.diaryId,
+                  diaryModel: widget.diaryModel,
                 )),
               );
             },
@@ -61,7 +110,7 @@ class DiaryDetailPage extends StatelessWidget {
                     // @ On Yes
                     MyButton(
                       onPressed: () {
-                        FirebaseHelper.deleteDiary(context, diaryId,
+                        FirebaseHelper.deleteDiary(context, widget.diaryId,
                             userController.currentUser.userId);
                       },
                       child: Text("Ya"),
@@ -85,6 +134,8 @@ class DiaryDetailPage extends StatelessWidget {
           SizedBox(width: 10),
         ],
       ),
+
+      // @ Body
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -92,10 +143,10 @@ class DiaryDetailPage extends StatelessWidget {
               padding: EdgeInsets.symmetric(horizontal: 15),
               child: Obx(
                 () => Text(
-                  diaryModel.isiDiary,
+                  widget.diaryModel.isiDiary,
                   style: TextStyle(
                     fontSize: detailController.fontSize,
-                    color: textColor,
+                    color: widget.textColor,
                   ),
                 ),
               ),
@@ -103,59 +154,86 @@ class DiaryDetailPage extends StatelessWidget {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          await showMyModalBottomSheet(
-            context,
-            [
-              DefaultTextStyle(
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Theme.of(context).textTheme.bodySmall!.color,
-                ),
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("Font Size"),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          // @ Edit Text Button
+          ElevatedButton(
+            onPressed: () async {
+              await showMyModalBottomSheet(
+                context,
+                [
+                  DefaultTextStyle(
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Theme.of(context).textTheme.bodySmall!.color,
+                    ),
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          IconButton(
-                            onPressed: () {
-                              double size = detailController.fontSize;
-                              if (size < 35) {
-                                size += 1;
-                                detailController.setFontSize(size);
-                              }
-                            },
-                            icon: Icon(Icons.text_increase_outlined),
-                          ),
-                          Obx(() => Text(
-                              detailController.fontSize.toInt().toString())),
-                          IconButton(
-                            onPressed: () {
-                              double size = detailController.fontSize;
-                              if (size > 7) {
-                                size -= 1;
-                                detailController.setFontSize(size);
-                              }
-                            },
-                            icon: Icon(Icons.text_decrease_outlined),
+                          Text("Font Size"),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  double size = detailController.fontSize;
+                                  if (size < 35) {
+                                    size += 1;
+                                    detailController.setFontSize(size);
+                                  }
+                                },
+                                icon: Icon(Icons.text_increase_outlined),
+                              ),
+                              Obx(() => Text(detailController.fontSize
+                                  .toInt()
+                                  .toString())),
+                              IconButton(
+                                onPressed: () {
+                                  double size = detailController.fontSize;
+                                  if (size > 7) {
+                                    size -= 1;
+                                    detailController.setFontSize(size);
+                                  }
+                                },
+                                icon: Icon(Icons.text_decrease_outlined),
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                    ],
+                    ),
                   ),
-                ),
-              ),
-              Divider(height: 0),
-            ],
-          );
-        },
-        child: Icon(Icons.text_format),
+                  Divider(height: 0),
+                ],
+              );
+            },
+            child: Icon(Icons.text_format),
+          ),
+
+          SizedBox(width: 10),
+
+          // @ Speech Button
+          ElevatedButton(
+            onPressed: () {
+              speak(widget.diaryModel.isiDiary);
+            },
+            child: Icon(Icons.spatial_audio_off_rounded),
+          ),
+        ],
       ),
     );
+  }
+
+  Future<void> speak(String text) async {
+    try {
+      // print(_currentVoice);
+      flutterTts.speak(text);
+    } catch (e) {
+      debugPrint("ERRRRRROOOOOR: $e");
+    }
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import app_links
 import firebase_core
 import firebase_database
+import flutter_tts
 import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
@@ -16,6 +17,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
